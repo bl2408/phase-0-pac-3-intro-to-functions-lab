ddc3b42097437837fc97e7a8542c5603d902d3e1
Brian Lambert <brianlambert2408@gmail.com>
2022-08-26 09:47:20 +1000

    Completed tasks

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout (str){
+    return str.toUpperCase();
+}
+
+function whisper(str){
+    return str.toLowerCase();
+}
+
+function logShout(str){
+    console.log(str.toUpperCase());
+}
+
+function logWhisper(str){
+    console.log(str.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(str){
+    switch(str){
+        case str.toLowerCase():
+            return "I can\'t hear you!";
+        case str.toUpperCase(): 
+            return "YES INDEED!";
+        case "Let\'s have dinner together!":
+            return "I would love to!";
+    }
+}
